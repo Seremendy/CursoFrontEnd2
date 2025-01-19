c11daa137102f6d6804d11c7875638e916242b49
Rodrigo Morello <rodrigomorello6@gmail.com>
2025-01-18 23:15:23 -0300

    adição do projeto ONE- oracle que estou cursando e seus projetos

---
+++ ONE-Alura/logica-js-projeto_inicial/app.js
@@ -0,0 +1,106 @@
+// alert('Boas vindas ao nosso site!.');
+// // let nome = ('Lua');
+// // let idade = 25;
+// let numeroDeVendas = 50;
+// let saldoDisponivel = 1000;
+// let mensagemDeErro = ('Erro! Preencha todos os campos');
+// alert(mensagemDeErro);
+
+// let nome = prompt('Qual é seu nome?');
+// let idade = prompt('Qual é sua idade?');
+
+// if (idade >= 18) {
+//     alert('Pode tirar a habilitação!');
+// }
+
+
+// EXERCICIO 1--------------------------------------------
+
+
+// let diaDaSemana = prompt('Qual é o dia da semana?');
+// if (diaDaSemana == 'Sábado') {
+//     alert('Bom fim de semana!');
+// } else if (diaDaSemana == 'Domingo') {
+//     alert('Bom fim de semana!');
+// } else {
+//     alert('Digite Sábado ou Domingo.')
+// }
+
+// EXERCICIO 2---------------------------------------------------
+
+
+// let verifiqueNumero = prompt('Qual é o número?');
+
+// if(verifiqueNumero >= 0) {
+//     alert('Seu número é positivo');
+// } else {
+//     alert('Seu número é negativo');
+// }
+
+
+// EXERCICIO 3--------------------------------------------------
+
+
+// let pontucao = prompt('Qual é sua pontuação?');
+
+// if (pontucao >= 100) {
+//     alert('Parabéns, você venceu!')
+// } else {
+//     alert('Tente novamente para ganhar.')
+// }
+
+
+// EXERCICIO 4------------------------------------------
+
+
+// let saldoDaConta = prompt('Qual é o saldo da conta?');
+// alert(`O saldo na conta é de ${saldoDaConta}`);
+
+
+// EXERCICIO 5---------------------------------------------------------------
+
+
+// let insiraNome = prompt('Qual é seu nome?');
+// alert(`Boas vindas ${insiraNome}`);
+
+
+// DESAFIOS while
+// Crie um contador que comece em 1 e vá até 10 usando um loop while. Mostre cada número.
+
+// let contador = 1;
+// while (contador <= 10) {
+//     console.log(contador);
+//     contador++;
+// } 
+
+
+// Crie um contador que começa em 10 e vá até 0 usando um loop while. Mostre cada número.
+
+// let contador = 10;
+// while (contador >= 0) {
+//     console.log(contador);
+//     contador--;
+// } 
+
+
+
+
+// Crie um programa de contagem regressiva. Peça um número e conte deste número até 0, usando um loop while no console do navegador.
+
+
+// let numeroMaximo = prompt('Digite um número para a contagem regressiva:');
+// while (contador >= 0) {
+//     console.log(contador);
+//     contador--;
+// } 
+
+
+
+// Crie um programa de contagem progressiva. Peça um número e conte de 0 até esse número, usando um loop while no console do navegador.
+// let numeroMaximo = prompt('Digite um número para a contagem progressiva:');
+// let contador = 0;
+
+// while (contador <= numeroMaximo) {
+//     console.log(contador);
+//     contador++;
+// } 
